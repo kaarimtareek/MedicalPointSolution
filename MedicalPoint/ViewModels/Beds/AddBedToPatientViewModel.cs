a812abc3d03d59186cbb0d9d708485a8c057bd3f
kimoo tareek <kimootareekk@outlook.com>
2023-09-02 10:35:43 +0300

    add bed to patient feature

---
+++ MedicalPoint/ViewModels/Beds/AddBedToPatientViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MedicalPoint.ViewModels.Patients;
+
+namespace MedicalPoint.ViewModels.Beds
+{
+    public class AddBedToPatientViewModel
+    {
+        public int Id { get; set; }
+        public string Notes { get; set; }
+        public List<PatientViewModel> Patients { get; set; }
+    }
+}
